a599e58da1a220e777395b63108f395111ee70b0
jossFBI <jossferib@gmail.com>
2026-08-20 22:56:49 -0500

    test cpp

---
+++ 1.dmy.cpp
@@ -0,0 +1,34 @@
+//Codigo en c++ de year-month-day
+#include<iostream>
+#include<random>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<ctime>
+
+using namespace std;
+
+//Variables
+char f_limit[] = "10062010";
+
+int extr_fech(char x, int y_f, int m_f, int d_f){
+    printf("%c", x);
+
+
+    return 0;
+}
+
+int main(){
+    int y = 0, m = 0, d = 0;
+    
+    extr_fech(' ', y, m, d);
+
+    for (y; y < 10;y++){
+        printf("XD");
+    }
+    
+    return 0;
+}
+
+
+
